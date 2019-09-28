c3296b54e2257d80320116b8818f7a496a7fe816
dreamingechoes <hi@dreamingecho.es>
2019-09-28 12:22:25 +0200

    WIP

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+const source_path = path.join(__dirname, 'assets');
+
+const plugins = [
+  new ExtractTextPlugin({
+    filename: 'stylesheets/[name].css',
+    allChunks: false,
+  }),
+];
+
+const optimization = {
+  splitChunks: {
+    cacheGroups: {
+      chunks: 'async',
+    },
+  },
+  concatenateModules: false,
+  minimizer: [],
+};
+
+module.exports = {
+  mode: isProd ? 'production' : 'development',
+  devtool: isProd ? false : 'source-map',
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  plugins,
+  optimization,
+  context: source_path,
+  entry: {
+    application: path.resolve(__dirname, 'assets/javascripts/site.js'),
+    styles: path.resolve(__dirname, 'assets/stylesheets/site.scss'),
+  },
+
+  output: {
+    path: path.resolve(__dirname, '.tmp/dist'),
+    filename: 'javascripts/[name].js',
+  },
+
+  resolve: {
+    modules: [
+      path.resolve(__dirname, '/assets'),
+      path.resolve(__dirname, '/node_modules'),
+    ],
+    extensions: ['.js, .scss, .sass, .css'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: [/node_modules/, /\.js$/],
+        use: [
+          {
+            loader: 'css-loader',
+            query: {
+              sourceMap: true,
+              importLoaders: 1,
+            },
+          },
+          // 'postcss-loader?sourceMap',
+          // {
+          //   loader: 'resolve-url-loader',
+          //   options: {
+          //     sourceMap: true,
+          //     // root: path.resolve(__dirname, 'assets/stylesheets'),
+          //   },
+          // },
+          {
+            loader: 'sass-loader?sourceMap',
+            options: {
+              sourceMap: true,
+              includePaths: ['node_modules'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
